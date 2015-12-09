8276884c2e74daaa549e29e08678a80d3d14d2a6
Matthew Berryman <mberryma@uow.edu.au>
2015-12-09 18:54:56 +1100

    Force https

--- config.js
+++ config.js
@@ -69,7 +69,7 @@ config.data = true; // Enable data routes
 config.aggregates = true; // Enable aggregate data outputs
 config.compression = false; // Enable express compression middleware
 
-config.enableHTTP = true;
+config.enableHTTP = false;
 
 // API settings
 config.api = {};
